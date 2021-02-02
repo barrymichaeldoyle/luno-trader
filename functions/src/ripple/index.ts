f347dadc7c5f116162cd1e1fb883eba64ac09725
Barry Michael Doyle <barry.doyle@universal.one>
2021-02-02 15:37:05 +0200

    added bulk functions (haven't tested bulk merge yet)

--- functions/src/ripple/index.ts
+++ functions/src/ripple/index.ts
@@ -7,6 +7,84 @@ import { fetchOrder, fetchPendingOrders } from '../api'
 import { Order, Trade } from '../interfaces'
 import { Authorization, color, selected, unselected } from '../utils'
 
+const bulkTask = async (taskType: 'CANCEL' | 'MERGE') => {
+  const orders = await fetchPendingOrders('XRPZAR')
+
+  interface OrderGroups {
+    [limit_price: string]: Array<Order>
+  }
+  const orderGroups: OrderGroups = {}
+  orders.forEach(order => {
+    if (orderGroups[order.limit_price])
+      orderGroups[order.limit_price].push(order)
+    else orderGroups[order.limit_price] = [order]
+  })
+
+  process.stdout.write(
+    color(
+      `Which group of orders would you like to ${taskType.toLowerCase()}?\n`,
+      'yellow'
+    )
+  )
+
+  interface Values {
+    [key: string]: string
+  }
+
+  const values: Values = {}
+  Object.keys(orderGroups).forEach(
+    group =>
+      (values[group] = `R ${group}\t ${color(
+        `${orderGroups[group].length} Order${
+          orderGroups[group].length === 1 ? '' : 's'
+        }`,
+        'cyan'
+      )}`)
+  )
+  values.cancel = 'Cancel'
+
+  const { id } = await select({
+    values,
+    selected,
+    unselected
+  })
+
+  let volume = 0
+
+  await Promise.all(
+    orderGroups[id].map(async ({ order_id, limit_volume }) => {
+      try {
+        const res = await fetch(
+          `https://api.luno.com/api/1/stoporder?order_id=${order_id}`,
+          {
+            method: 'POST',
+            headers: { Authorization }
+          }
+        )
+        const json = await res.json()
+        if (json.success)
+          process.stdout.write(
+            `${color('Successfully Cancelled', 'green')} ${color(
+              `ORDER ${order_id}\n`,
+              'yellow'
+            )}`
+          )
+        if (taskType === 'MERGE') volume = volume + Number(limit_volume)
+      } catch (e) {
+        process.stderr.write(`Failed to Stop Order: ${e.message}`)
+      }
+    })
+  )
+
+  if (taskType === 'MERGE') {
+    const type = orderGroups[id][0].type
+    const price = orderGroups[id][0].limit_price
+    const res = await fetch(
+      `https://api.luno.com/api/1/postorder?pair=XRPZAR&type=${type}&volume=${volume}&price=${price}`
+    )
+  }
+}
+
 const openNewOrder = async (
   type: 'ASK' | 'BID',
   price: string,
@@ -80,7 +158,7 @@ const fetchNewTrades = async (
           `${color('Trade: ', 'cyan')} ${color(type, 'green')} ${color(
             Number(volume).toFixed(0),
             'white'
-          )} ${color(`@ R${price}`, 'yellow')}`
+          )} ${color(`@ R${price}`, 'yellow')}\n`
         )
         newDoneStamps.push(timestamp)
 
@@ -230,11 +308,12 @@ const main = async () => {
         continue
       }
       case 'bulkCancel': {
-        process.stdout.write(
-          color('\nBULK CANCELS ARE A WORK IN PROGRESS\n', 'cyan')
-        )
+        await bulkTask('CANCEL')
         continue
       }
+      case 'bulkMerge':
+        await bulkTask('MERGE')
+        continue
       case 'refresh':
         continue
 
