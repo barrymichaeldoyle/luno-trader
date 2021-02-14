fde5fdd3f330429df43aa41870a5d35fbc073be4
Barry Michael Doyle <barry.doyle@universal.one>
2021-02-14 12:21:56 +0200

    cleaned up trade functions

--- functions/src/luno/logs/printTrade.ts
+++ functions/src/luno/logs/printTrade.ts
@@ -2,15 +2,20 @@ import { color } from '../../logs'
 import { getUnitPrecision } from '../common'
 import { PAIR, TYPE } from '../interfaces'
 
+const tradePair = (pair: PAIR) => color(`Trade ${pair}:`, 'cyan')
+const tradeType = (type: TYPE) =>
+  color(type === 'ASK' ? 'SOLD' : 'BOUGHT', type === 'ASK' ? 'red' : 'green')
+const tradeVolume = (volume: string, pair: PAIR) =>
+  color(Number(volume).toFixed(getUnitPrecision(pair)), 'white')
+const tradePrice = (price: string) =>
+  color(`@ R${Number(price).toFixed(2)}`, 'yellow')
+
 const printTrade = (pair: PAIR, type: TYPE, price: string, volume: string) =>
   process.stdout.write(
-    `${color(`Trade ${pair}: `, 'cyan')} ${color(
-      type === 'ASK' ? 'SOLD' : 'BOUGHT',
-      type === 'ASK' ? 'red' : 'green'
-    )} ${color(
-      Number(volume).toFixed(getUnitPrecision(pair)),
-      'white'
-    )} ${color(`@ R${Number(price).toFixed(2)}`, 'yellow')}\n`
+    `${tradePair(pair)} ${tradeType(type)} ${tradeVolume(
+      volume,
+      pair
+    )} ${tradePrice(price)}\n`
   )
 
 export default printTrade
